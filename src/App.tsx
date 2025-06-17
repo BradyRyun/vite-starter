16e8ce66820846159446c1501b395d34fdbef326
Brady Ryun <39814792+BradyRyun@users.noreply.github.com>
2025-06-17 23:21:32 +0200

    upgrade to tailwindcss 4

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,9 @@
-import { Button } from "@/components/ui/button";
 import './App.css';
 
 function App() {
   return (
     <>
     <div>
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
-    <Button>Click me</Button>
     </div>
     </>
   )
